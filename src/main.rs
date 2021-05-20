fbd2e7148d80ebca957d827c0af09a129a96e6cf
vallisneria <moogi3469@gmail.com>
2021-05-20 16:12:38 +0900

    작업이 2분마다 실행되도록 코드 변경

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
+use std::thread;
+use std::time::Duration;
+
 mod saramin;
 use saramin::web::get_job_list as get_saramin;
 
 fn main() {
-    // 데이터 가져오기
-    let saramin_list = get_saramin(0).unwrap();
+    loop {
+        // 데이터 가져오기
+        let saramin_list = get_saramin(0).unwrap();
 
-    // 가져온 데이터 처리
-    for data in saramin_list.iter() {
-        println!("{}\n\n", data.to_string());
+        // 가져온 데이터 처리
+        for data in saramin_list.iter() {
+            println!("{}\n\n", data.to_string());
+        }
+
+        // 가장 최근 데이터를 저장하기
+        format!(
+            "INSERT INTO saramin(id, company_name, title) VALUES ({id}, {company_name}, {title})",
+            id = &saramin_list[0].id,
+            company_name = &saramin_list[0].company_name,
+            title = &saramin_list[0].title
+        );
+
+        // 작업이 모두 끝나면 2분동안 대기
+        thread::sleep(Duration::from_secs(120));
     }
-
-    // 가장 최근 데이터를 저장하기
-    format!(
-        "INSERT INTO saramin(id, company_name, title) VALUES ({id}, {company_name}, {title})",
-        id = &saramin_list[0].id,
-        company_name = &saramin_list[0].company_name,
-        title = &saramin_list[0].title
-    );
 }
