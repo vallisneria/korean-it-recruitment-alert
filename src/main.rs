55e838cd51e2878dad9d83d8b55b08e8cdb03ac5
vallisneria <moogi3469@gmail.com>
2021-07-18 01:53:29 +0900

    기초 작성

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+type Result<T> = Result<T, Box<dyn std::error::Error>>;
+
 fn main() {
-    println!("Hello, world!");
+    // init
+
+    loop {
+        // 각 취업정보 사이트 작업
+
+        // 일정 시간동안 대기
+    }
+}
+
+fn saramin() -> Result<()> {
+    // 데이터 가져오기
+
+    // 데이처 처리
+
+    // 다음 호출에서 사용할 수 있도록
+    // 마지막 데이터 저장
+
+    Ok(())
+}
+
+fn jobkorea() -> Result<()> {
+    // 데이터 가져오기
+
+    // 데이터 처리
+
+    // 다음 호출에서 사용할 수 있도록
+    // 마지막 데이터 저장
+
+    Ok(())
+}
+
+fn incruit() -> Result<()> {
+    // 데이터 가져오기
+
+    // 데이터 처리
+
+    // 다음 호출에서 사용할 수 있도록
+    // 마지막 데이터 저장
+
+    Ok(())
 }
