f8e027b53264bc6cdc265b5e3b6c67ea6fbfb303
vallisneria <moogi3469@gmail.com>
2021-07-22 01:14:59 +0900

    응답을 Document 객체로 변환할 수 없었던 문제 수정

--- src/saramin/fetch.rs
+++ src/saramin/fetch.rs
@@ -4,7 +4,8 @@ use select::document::Document;
 pub async fn fetch() -> Result<Document, Box<dyn std::error::Error>> {
     const SARAMIN_URL: &str =
         "https://www.saramin.co.kr/zf_user/jobs/list/job-category?cat_bcd=4&sort=MD";
-    let response = reqwest::get(SARAMIN_URL).await?;
+    let response = reqwest::get(SARAMIN_URL).await?.text().await?;
+    let document = Document::from_read(response.as_bytes())?;
 
-    Document::from_read(response)?
+    Ok(document)
 }
