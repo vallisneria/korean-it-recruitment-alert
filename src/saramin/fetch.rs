4f2a7d012932805cb15827d313724302321beeca
vallisneria <moogi3469@gmail.com>
2021-07-29 18:48:53 +0900

    `fetch` 함수가`reqwest::Client`를 빌려와 사용하도록 변경

--- src/saramin/fetch.rs
+++ src/saramin/fetch.rs
@@ -1,10 +1,10 @@
 use reqwest;
 use select::document::Document;
 
-pub async fn fetch() -> Result<Document, Box<dyn std::error::Error>> {
+pub async fn fetch(http_client: &reqwest::Client) -> Result<Document, Box<dyn std::error::Error>> {
     const SARAMIN_URL: &str =
         "https://www.saramin.co.kr/zf_user/jobs/list/job-category?cat_bcd=4&sort=MD";
-    let response = reqwest::get(SARAMIN_URL).await?.text().await?;
+    let response = http_client.get(SARAMIN_URL).send().await?.text().await?;
     let document = Document::from_read(response.as_bytes())?;
 
     Ok(document)
