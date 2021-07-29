a94cd747fe9b2ae52203fc890b769081002968b9
vallisneria <moogi3469@gmail.com>
2021-07-30 01:08:04 +0900

    main 함수 초기화

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,9 @@
-fn main() {
-    // init
+mod jobkorea;
+mod saramin;
 
-    loop {
-        // 각 취업정보 사이트 작업
+use reqwest;
 
-        // 일정 시간동안 대기
-    }
-}
-
-fn saramin_work() -> Result<(), Box<dyn std::error::Error>> {
-    // 데이터 가져오기
-
-    // 데이처 처리
-
-    // 다음 호출에서 사용할 수 있도록
-    // 마지막 데이터 저장
-
-    Ok(())
-}
-
-fn jobkorea_work() -> Result<(), Box<dyn std::error::Error>> {
-    // 데이터 가져오기
-
-    // 데이터 처리
-
-    // 다음 호출에서 사용할 수 있도록
-    // 마지막 데이터 저장
-
-    Ok(())
+#[tokio::main]
+async fn main() {
+    // do something
 }
