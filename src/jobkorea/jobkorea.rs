a1a9182224a6961cb931cea110060a043afb31fd
vallisneria <moogi3469@gmail.com>
2021-08-09 01:40:10 +0900

    `JobKorea` 구조체 일부 멤버 변수 공개 범위 변경

--- src/jobkorea/jobkorea.rs
+++ src/jobkorea/jobkorea.rs
@@ -1,11 +1,23 @@
 use crate::posting::mastodon::MastodonUpload;
 
+/// 잡코리아 채용 공고
 pub struct JobKorea {
-    id: u32,
+    /// 공고 id
+    pub id: u32,
+
+    /// 회사명
     company_name: String,
-    title: String,
+
+    /// 공고 제목
+    pub title: String,
+
+    /// 경력, 학력, 근무지 등 정보
     info: Vec<String>,
+
+    /// 모집기한
     deadline: String,
+
+    /// 링크
     link: String,
 }
 
