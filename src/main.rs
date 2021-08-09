f404708b1082abe4b13cf54784213153128fa675
vallisneria <moogi3469@gmail.com>
2021-08-09 17:57:42 +0900

    `jobkorea` 적용

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,26 @@ use tokio::time::{sleep, Duration};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let mstdn_enable = env::var("MASTODON_ENABLE").is_ok();
-    let mstdn = posting::mastodon::Mastodon::new(
-        env::var("MASTODON_URL")?,
-        env::var("MASTODON_BEARER_TOKEN")?,
-    );
+    let mstdn = if mstdn_enable {
+        println!("[log] MASTODON ENABLE");
+        Some(posting::mastodon::Mastodon::new(
+            env::var("MASTODON_URL")?,
+            env::var("MASTODON_BEARER_TOKEN")?,
+        ))
+    } else {
+        println!("[log] MASTODON DISABLE");
+        None
+    };
     let http_client = reqwest::Client::new();
     let mut latest_saramin_id = saramin::init(&http_client).await?;
+    let mut latest_jobkorea_id = jobkorea::init(&http_client).await?;
 
     loop {
         println!("[log] latest_saramin_id={}", latest_saramin_id);
+        println!("[log] latest_jobkorea_id={}", latest_jobkorea_id);
         sleep(Duration::from_millis(120_000)).await;
 
         latest_saramin_id = saramin::cycle(latest_saramin_id, &http_client, &mstdn).await?;
+        latest_jobkorea_id = jobkorea::cycle(latest_jobkorea_id, &http_client, &mstdn).await?;
     }
 }
--- src/saramin/mod.rs
+++ src/saramin/mod.rs
@@ -16,7 +16,7 @@ pub async fn init(http_client: &reqwest::Client) -> Result<u32, Box<dyn Error>> 
 /// 채용 공고를 가져오고 업로드함
 /// 가장 최근의 채용 공고 id를 반환한다.
 pub async fn cycle(
-    latest_id: u32, http_client: &reqwest::Client, mstdn: &Mastodon,
+    latest_id: u32, http_client: &reqwest::Client, mstdn: &Option<Mastodon>,
 ) -> Result<u32, Box<dyn Error>> {
     // 웹 사이트에서 목록을 가져옴
     let saramin_document = fetch::fetch(&http_client).await?;
@@ -26,8 +26,14 @@ pub async fn cycle(
     // 가져온 공고를 처리함
     for i in data.iter() {
         if i.id > latest_id {
-            println!("[{}] {}", i.id, i.title);
-            mstdn.posting(&http_client, i).await?;
+            println!("[saramin/{}] {}", i.id, i.title);
+
+            match mstdn {
+                Some(m) => {
+                    m.posting(&http_client, i).await?;
+                }
+                None => {}
+            }
         } else {
             break;
         }
