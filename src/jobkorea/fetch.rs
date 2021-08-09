063f4528260a8379022ce51321581755f97bd431
vallisneria <moogi3469@gmail.com>
2021-08-09 17:56:59 +0900

    사람인에서 데이터 가져오는 방식 변경

--- src/jobkorea/fetch.rs
+++ src/jobkorea/fetch.rs
@@ -1,21 +1,36 @@
-use reqwest::header::{HeaderMap, HeaderValue};
 use reqwest::Client;
 use select::document::Document;
 use std::error::Error;
 
 pub async fn fetch(http_client: &Client) -> Result<Document, Box<dyn Error>> {
-    const COOKIE: &str = r#"CookieNo=1284391680; PCID=16201107319712787862745; mainContents=0; MainLayer=done; MainRcntlyData=%3c%6c%69%3e%3c%61%20%68%72%65%66%3d%22%2f%72%65%63%72%75%69%74%2f%6a%6f%62%6c%69%73%74%3f%6d%65%6e%75%63%6f%64%65%3d%64%75%74%79%22%3e%c1%f7%b9%ab%ba%b0%3c%2f%61%3e%20%26%67%74%3b%20%3c%61%20%68%72%65%66%3d%22%2f%72%65%63%72%75%69%74%2f%6a%6f%62%6c%69%73%74%3f%6d%65%6e%75%63%6f%64%65%3d%64%75%74%79%26%64%75%74%79%43%74%67%72%3d%31%30%30%31%36%22%20%63%6c%61%73%73%3d%22%63%61%74%65%22%3e%49%54%a1%a4%c0%ce%c5%cd…4,1000109","Employ_Type":"1,2,3,9","Reg_Dt":"2021-08-08T17:28:15.383876+09:00","IsKeep":true},{"CookieIndex":"20210504161249","Cndt_No":0,"M_Id":"","Employ_Type":"9","Reg_Dt":"2021-05-04T16:12:49.899369+09:00","IsKeep":true}]; DirectStat=ON; ASP.NET_SessionId=lkvttvjp113mcxwwx20dq5ku; jobkorea=Site_Oem_Code=C1; ECHO_SESSION=5301628411212157; Main_Top_Banner_Seq=1; rstn_item=%7B%22duty%22%3A%7B%22item1%22%3A%20%7B%22code%22%3A%2210016%22%2C%22name%22%3A%22IT%B7%uC778%uD130%uB137%22%2C%22sort%22%3A1%7D%7D%7D"#;
     const URL: &str = "https://www.jobkorea.co.kr/Recruit/Home/_GI_List/";
+    #[rustfmt::skip]
+    let body = [
+        ("isDefault", "true"),
+        ("condition[duty]", "1000100,1000101,1000102,1000109,1000094,1000097",),
+        ("condition[jobtype]", "1,3,9"),
+        ("condition[menucode]", ""),
+        ("page", "1"),
+        ("direct", "0"),
+        ("order", "2"),
+        ("pagesize", "20"),
+        ("tabindex", "0"),
+        ("fulltime", "0"),
+        ("confirm", "0"),
+    ];
 
-    http_client
+    #[rustfmt::skip]
+    let res = http_client
         .post(URL)
-        .header("Cookie", COOKIE)
+        .header("Accept", "text/html, */*; q=0.01")
+        .header("Referer", "https://www.jobkorea.co.kr/recruit/joblist?menucode=duty&dutyCtgr=10016",)
+        .header("X-Requested-With", "XMLHttpRequest")
+        .header("DNT", "1")
+        .form(&body)
         .send()
-        .await?
-        .text()
         .await?;
 
-    let document = Document::from_read(String::new().as_bytes())?;
+    let document = Document::from_read(res.text().await?.as_bytes())?;
     Ok(document)
 }
 
