c20ee5714d627cd97dc9e5845ec464581a487bbe
vallisneria <moogi3469@gmail.com>
2021-08-11 14:58:35 +0900

    정규표현식 이용해서 필드 분류하도록 수정

--- src/jobkorea/data_extraction.rs
+++ src/jobkorea/data_extraction.rs
@@ -1,4 +1,5 @@
 use super::jobkorea::JobKorea;
+use regex::Regex;
 use scraper::{ElementRef, Html, Selector};
 
 pub fn data_extract(document: &Html) -> Vec<JobKorea> {
@@ -6,11 +7,17 @@ pub fn data_extract(document: &Html) -> Vec<JobKorea> {
     let selector = Selector::parse("tr.devloopArea").unwrap();
 
     for element in document.select(&selector) {
+        let info = get_info(element);
         let data = JobKorea::new(
             get_id(element),
             get_company_name(element),
             get_title(element),
-            get_info(element),
+            get_career(&info),
+            get_education(&info),
+            get_work_place(&info),
+            get_employment_type(&info),
+            get_salary(&info),
+            get_position(&info),
             get_deadline(element),
             get_link(element),
         );
@@ -60,12 +67,81 @@ fn get_title(element: ElementRef) -> String {
         .unwrap()
 }
 
-fn get_info(element: ElementRef) -> Vec<String> {
+fn get_info(element: ElementRef) -> Vec<&str> {
     let selector = Selector::parse(".cell").unwrap();
     element
         .select(&selector)
-        .map(|x| x.text().next().unwrap_or("").parse().unwrap())
+        .map(|x| x.text().next().unwrap_or(""))
         .collect()
+}
+
+fn _reg_filter(reg: Regex, info: &Vec<&str>) -> Vec<String> {
+    info.iter()
+        .filter(|x| reg.is_match(x))
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+}
+
+fn get_career(info: &Vec<&str>) -> Option<String> {
+    let career_reg = Regex::new(r"(경력\S*|신입\S*)").unwrap();
+    let filter = _reg_filter(career_reg, info);
+
+    match filter.len() {
+        1 => Some(filter[0].clone()),
+        _ => None,
+    }
+}
+
+fn get_education(info: &Vec<&str>) -> Option<String> {
+    let edu_reg = Regex::new(r"(학력무관|(고|초?대)졸|[석박]사)\S*").unwrap();
+    let filter = _reg_filter(edu_reg, info);
+
+    match filter.len() {
+        1 => Some(filter[0].clone()),
+        _ => None,
+    }
+}
+
+fn get_work_place(info: &Vec<&str>) -> Option<String> {
+    let place_reg = Regex::new(
+        r"(서울|인천|부산|울산|대전|세종|광주|대구|경기|강원|충[남북]|경[남북]|전[남북]|제주|전국|미국|아시아|일본|중국|[남북]아메리카|오세아니아|아프리카|유럽).*",
+    ).unwrap();
+    let filter = _reg_filter(place_reg, info);
+
+    match filter.len() {
+        1 => Some(filter[0].clone()),
+        _ => None,
+    }
+}
+
+fn get_employment_type(info: &Vec<&str>) -> Option<String> {
+    let employment_reg = Regex::new(r"((정규|계약|파견|위촉)직|(연수|교육)생|인턴|도급|프리랜서|아르바이트|병역특례|개인사업자).*").unwrap();
+    let filter = _reg_filter(employment_reg, info);
+
+    match filter.len() {
+        1 => Some(filter[0].clone()),
+        _ => None,
+    }
+}
+
+fn get_salary(info: &Vec<&str>) -> Option<String> {
+    let salary_reg = Regex::new(r"[0-9,~]+.*").unwrap();
+    let filter = _reg_filter(salary_reg, info);
+
+    match filter.len() {
+        1 => Some(filter[0].clone()),
+        _ => None,
+    }
+}
+
+fn get_position(info: &Vec<&str>) -> Option<String> {
+    let position_reg = Regex::new(r"(사원|주임|대리|과장|차장|부장|임원|CEO).*").unwrap();
+    let filter = _reg_filter(position_reg, info);
+
+    match filter.len() {
+        1 => Some(filter[0].clone()),
+        _ => None,
+    }
 }
 
 fn get_deadline(element: ElementRef) -> String {
--- src/jobkorea/jobkorea.rs
+++ src/jobkorea/jobkorea.rs
@@ -11,8 +11,23 @@ pub struct JobKorea {
     /// 공고 제목
     pub title: String,
 
-    /// 경력, 학력, 근무지 등 정보
-    info: Vec<String>,
+    /// 경력
+    career: Option<String>,
+
+    /// 학력
+    education: Option<String>,
+
+    /// 근무지
+    work_place: Option<String>,
+
+    /// 근로형태
+    employment_type: Option<String>,
+
+    /// 임금
+    salary: Option<String>,
+
+    /// 직급
+    position: Option<String>,
 
     /// 모집기한
     deadline: String,
@@ -23,14 +38,20 @@ pub struct JobKorea {
 
 impl JobKorea {
     pub fn new(
-        id: u32, company_name: String, title: String, info: Vec<String>, deadline: String,
-        link: String,
+        id: u32, company_name: String, title: String, career: Option<String>,
+        education: Option<String>, work_place: Option<String>, employment_type: Option<String>,
+        salary: Option<String>, position: Option<String>, deadline: String, link: String,
     ) -> JobKorea {
         JobKorea {
             id,
             company_name,
             title,
-            info,
+            career,
+            education,
+            work_place,
+            employment_type,
+            salary,
+            position,
             deadline,
             link,
         }
@@ -43,16 +64,26 @@ impl MastodonUpload for JobKorea {
     }
 
     fn status(&self) -> String {
+        let none = String::from("-");
         format!(
             r"🏢 {company_name}
 
-{info}
-
+⌛ {career}
+🏫 {education}
+💼 {employment_type}
+🗺️ {work_place}
+💰 {salary}
+👔 {position}
 🕑 {deadline}
 
 🔗 {link}",
             company_name = &self.company_name,
-            info = &self.info.join(" / "),
+            career = &self.career.as_ref().unwrap_or(&none),
+            education = &self.education.as_ref().unwrap_or(&none),
+            employment_type = &self.employment_type.as_ref().unwrap_or(&none),
+            work_place = &self.work_place.as_ref().unwrap_or(&none),
+            salary = &self.salary.as_ref().unwrap_or(&none),
+            position = &self.position.as_ref().unwrap_or(&none),
             deadline = &self.deadline,
             link = &self.link
         )
